0ba94f2b43ffbe529d89cf70ba9985354e59500c
Carl Hetherington <cth@carlh.net>
2012-04-23 19:06:11 +0100

    Various work.

--- make.sh
+++ make.sh
@@ -5,6 +5,16 @@ for f in diagrams/*.svg; do
     inkscape -z -f $f --export-pdf $g --export-area-drawing
 done
 
+for f in graphics/*.svg; do
+    g=`echo $f | sed -e "s/.svg/.pdf/"`
+    inkscape -z -f $f --export-pdf $g --export-area-drawing
+done
+
+for f in graphs/*.dot; do
+    g=`echo $f | sed -e "s/.dot//"`
+    dot2tex --preproc $f | dot2tex -t raw --figonly -- > $g.tex
+done
+
 pdflatex dev_manual.tex && pdflatex dev_manual.tex && pdflatex dev_manual.tex
 
 if [ "$1" == "-d" ]; then
